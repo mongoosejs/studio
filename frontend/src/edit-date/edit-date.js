c44e705b24e3e3f6d8442feee6fe812ee3ce8cb2
Valeri Karpov <val@karpov.io>
2023-04-10 16:01:47 -0400

    Update edit-date.js

--- frontend/src/edit-date/edit-date.js
+++ frontend/src/edit-date/edit-date.js
@@ -5,16 +5,5 @@ const template = require('./edit-date.html');
 module.exports = app => app.component('edit-date', {
   template: template,
   props: ['value'],
-  emits: ['input'],
-  computed: {
-    displayValue() {
-      if (this.value === null) {
-        return 'null';
-      }
-      if (this.value === undefined) {
-        return 'undefined';
-      }
-      return this.value;
-    }
-  }
+  emits: ['input']
 });
