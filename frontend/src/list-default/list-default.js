a85c6176aa91544a38085aea3b4565492ba19fc1
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2023-04-21 14:54:22 -0400

    copy icon/functionality with tooltip

--- frontend/src/list-default/list-default.js
+++ frontend/src/list-default/list-default.js
@@ -1,10 +1,27 @@
 'use strict';
 
 const template = require('./list-default.html');
+const appendCSS = require('../appendCSS');
+
+appendCSS(require('./list-default.css'));
 
 module.exports = app => app.component('list-default', {
   template: template,
   props: ['value'],
+  methods: {
+    copyText(value) {
+      console.log('I have been clicked!')
+      const storage = document.createElement('textarea');
+      storage.value = value;
+      const elem = document.querySelector('#value-field');
+      elem.appendChild(storage);
+      storage.select();
+      storage.setSelectionRange(0, 99999);
+      document.execCommand('copy');
+      elem.removeChild(storage);
+
+    }
+  },
   computed: {
     displayValue() {
       if (this.value === null) {
