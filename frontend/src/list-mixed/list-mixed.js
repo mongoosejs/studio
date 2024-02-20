25d24d19fe4c5a9c761841274d8bf05241d3872d
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2024-02-20 11:28:59 -0500

    add copy functionality to embeds and mixed

--- frontend/src/list-mixed/list-mixed.js
+++ frontend/src/list-mixed/list-mixed.js
@@ -3,6 +3,8 @@
 const api = require('../api');
 const template = require('./list-mixed.html');
 
+const vanillatoast = require('vanillatoasts');
+
 require('../appendCSS')(require('./list-mixed.css'));
 
 module.exports = app => app.component('list-mixed', {
@@ -13,7 +15,27 @@ module.exports = app => app.component('list-mixed', {
       return this.value;
     }
   },
+  methods: {
+    copyText(value) {
+      const storage = document.createElement('textarea');
+      storage.value = JSON.stringify(value);
+      const elem = this.$refs.MixedCode;
+      elem.appendChild(storage);
+      storage.select();
+      storage.setSelectionRange(0, 99999);
+      document.execCommand('copy');
+      elem.removeChild(storage);
+      vanillatoast.create({
+        title: 'Text copied!',
+        type: 'success',
+        timeout: 3000,
+        icon: 'images/success.png',
+        positionClass: 'bottomRight'
+      });
+    }
+  },
   mounted: function() {
     Prism.highlightElement(this.$refs.MixedCode);
   }
 });
+
