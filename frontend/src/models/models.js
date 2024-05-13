45ccb5c7f71bf8cd07bb820521a8be597a66ee0e
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2024-05-13 17:33:38 -0400

    enable filtering

--- frontend/src/models/models.js
+++ frontend/src/models/models.js
@@ -81,6 +81,14 @@ module.exports = app => app.component('models', {
     this.status = 'loaded';
   },
   methods: {
+    filterDocument(doc) {
+      const filteredDoc = {};
+      console.log(doc, this.filteredPaths)
+      for (let i = 0; i < this.filteredPaths.length; i++) {
+        filteredDoc[this.filteredPaths[i].path] = doc[this.filteredPaths[i].path];
+      }
+      return filteredDoc;
+    },
     async onScroll() {
       if (this.status === 'loading' || this.loadedAllDocs) {
         return;
