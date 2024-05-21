659f8f75054bc9d14677dac2c78c391c80f1017a
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2024-05-21 12:55:26 -0400

    edit arrays

--- frontend/src/edit-array/edit-array.js
+++ frontend/src/edit-array/edit-array.js
@@ -2,23 +2,52 @@
 
 const template = require('./edit-array.html');
 
+const { EditorState } = require('@codemirror/state');
+const { lineNumbers } = require('@codemirror/view')
+const { EditorView, basicSetup } = require('codemirror');
+const { javascript } = require('@codemirror/lang-javascript');
+
+const { BSON, EJSON } = require('bson');
+
+const ObjectId = new Proxy(BSON.ObjectId, {
+  apply (target, thisArg, argumentsList) {
+    return new target(...argumentsList);
+  }
+});
+
 const appendCSS = require('../appendCSS');
 appendCSS(require('./edit-array.css'));
 
 module.exports = app => app.component('edit-array', {
   template: template,
   props: ['value'],
-  data: () => ({ currentValue: null }),
+  data: () => ({ currentValue: null, editor: null }),
   mounted() {
     this.currentValue = this.value;
+    this.editor = new EditorView({
+      state: EditorState.create({
+        doc: this.currentValue.join('\n'),
+        extensions: [
+          basicSetup,
+          javascript(),
+          // history(),
+          EditorView.updateListener.of((v) => {
+            // Your update logic here
+          }),
+          // keymap.of(historyKeymap)
+        ]
+      }),
+      parent: this.$refs.arrayEditor
+    });
   },
   methods: {
     onUpdate() {
-      this.$emit('input', this.currentValue);
+      this.$emit('input', this.currentValue.split('\n'));
     },
-    removeValue(i) {
-      this.currentValue.splice(i, 1);
-      this.$emit('input', this.currentValue);
+  },
+  beforeDestroy() {
+    if (this.editor) {
+      this.editor.toTextArea();
     }
   },
   emits: ['input']
