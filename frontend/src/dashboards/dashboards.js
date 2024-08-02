edfcdbb0888f6cbb85738199ad5c3764c3dbd71b
Daniel Diaz <39510674+IslandRhythms@users.noreply.github.com>
2024-08-02 12:34:42 -0400

    allow deleting document + bug fixes

--- frontend/src/dashboards/dashboards.js
+++ frontend/src/dashboards/dashboards.js
@@ -9,8 +9,28 @@ module.exports = app => app.component('dashboards', {
   data: () => ({
     status: 'loading',
     dashboards: [],
-    showCreateDashboardModal: false
+    showCreateDashboardModal: false,
+    showDeleteDashboardModal: null
   }),
+  methods: {
+    async deleteDashboard(dashboard) {
+      if (!dashboard) {
+        return;
+      }
+      const { result } = await api.Dashboard.deleteDashboard({ dashboardId: dashboard._id });
+      if (result.deletedCount > 0) {
+        const removedDashboard = this.dashboards.findIndex(x => x._id.toString() === dashboard._id.toString());
+        this.dashboards.splice(removedDashboard, 1);
+        this.showDeleteDashboardModal = null;
+      } else {
+        throw new Error('Dashboard unable to be deleted');
+      }
+    },
+    insertNewDashboard(dashboard) {
+      this.dashboards.push(dashboard);
+      this.showCreateDashboardModal = false;
+    }
+  },
   async mounted() {
     const { dashboards } = await api.Dashboard.getDashboards();
     this.dashboards = dashboards;
