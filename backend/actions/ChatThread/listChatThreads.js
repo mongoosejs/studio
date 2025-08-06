87bf42d5716291439414c9c835d62802e1479fb0
Valeri Karpov <val@karpov.io>
2025-08-06 14:14:53 -0400

    feat(chat): track workspace sharing state

--- backend/actions/ChatThread/listChatThreads.js
+++ backend/actions/ChatThread/listChatThreads.js
@@ -10,18 +10,26 @@ const ListChatThreadsParams = new Archetype({
   },
   roles: {
     $type: ['string']
+  },
+  $workspaceId: {
+    $type: mongoose.Types.ObjectId
   }
 }).compile('ListChatThreadsParams');
 
 module.exports = ({ db, studioConnection }) => async function listChatThreads(params) {
   // Validate the params object
-  const { userId, roles } = new ListChatThreadsParams(params);
+  const { userId, roles, $workspaceId } = new ListChatThreadsParams(params);
   const ChatThread = studioConnection.model('__Studio_ChatThread');
 
   await authorize('ChatThread.listChatThreads', roles);
 
+  const query = { $or: [{ userId }] };
+  if ($workspaceId) {
+    query.$or.push({ workspaceId: $workspaceId, 'sharingOptions.sharedWithWorkspace': true });
+  }
+
   // Get all chat threads
-  const chatThreads = await ChatThread.find(userId ? { userId } : {})
+  const chatThreads = await ChatThread.find(query)
     .sort({ updatedAt: -1 }); // Sort by most recently updated
 
   return {
