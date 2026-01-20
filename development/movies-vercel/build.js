f2b579140d55fab47bb5cae85da369791a605df6
Valeri Karpov <val@karpov.io>
2026-01-20 16:48:23 -0500

    add beginning of vercel test deployment

---
+++ development/movies-vercel/build.js
@@ -0,0 +1,16 @@
+'use strict';
+
+require('../../../frontend')(`/api/studio`, true, opts)
+  .then(() => {
+    execSync(
+      `
+      mkdir -p ./public
+      cp -r ./../../../frontend/public/* ./public/
+      `
+    );
+    console.log('Built Mongoose Studio frontend');
+  })
+  .catch(err => {
+    console.error('Failed to build Mongoose Studio frontend', err);
+    process.exit(1);
+  });
