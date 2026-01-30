17eaf5e66093b52054c52f296e886426623642f6
Valeri Karpov <val@karpov.io>
2026-01-30 17:25:16 -0500

    fix evaluating dashboards on nextjs

--- backend/next.js
+++ backend/next.js
@@ -5,9 +5,66 @@ const Backend = require('./');
 module.exports = function next(conn, options) {
   const backend = Backend(conn, options?.studioConnection, options);
 
-  return function wrappedNextJSFunction(req, res) {
+  const mothershipUrl = options?._mothershipUrl || 'https://mongoose-js.netlify.app/.netlify/functions';
+  let workspace = null;
+
+  return async function wrappedNextJSFunction(req, res) {
     const params = { ...req.query, ...req.body, ...req.params };
     const actionName = params?.action;
+
+    const authorization = params?.authorization;
+    if (options?.apiKey) {
+      if (!authorization) {
+        throw new Error('Not authorized');
+      }
+
+      if (workspace == null) {
+        ({ workspace } = await fetch(`${mothershipUrl}/getWorkspace`, {
+          method: 'POST',
+          body: JSON.stringify({ apiKey: options.apiKey }),
+          headers: {
+            Authorization: `Bearer ${options.apiKey}`,
+            'Content-Type': 'application/json'
+          }
+        })
+          .then(response => {
+            if (response.status < 200 || response.status >= 400) {
+              return response.json().then(data => {
+                throw new Error(`Error getting workspace ${response.status}: ${require('util').inspect(data)}`);
+              });
+            }
+            return response;
+          })
+          .then(res => res.json()));
+      }
+
+      const { user, roles } = await fetch(`${mothershipUrl}/me?`, {
+        method: 'POST',
+        body: JSON.stringify({ workspaceId: workspace._id }),
+        headers: {
+          Authorization: authorization,
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => {
+          if (response.status < 200 || response.status >= 400) {
+            return response.json().then(data => {
+              throw new Error(`Mongoose Studio API Key Error ${response.status}: ${require('util').inspect(data)}`);
+            });
+          }
+          return response;
+        })
+        .then(res => res.json());
+      if (!user || !roles) {
+        throw new Error('Not authorized');
+      }
+
+      params.$workspaceId = workspace._id;
+      params.roles = roles;
+      params.userId = user._id;
+      params.initiatedById = user._id;
+    }
+
     if (typeof actionName !== 'string') {
       throw new Error('No action specified');
     }
