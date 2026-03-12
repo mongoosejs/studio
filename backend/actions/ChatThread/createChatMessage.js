6d66059d56c3741b36a9605fdc0a95049f686199
Valeri Karpov <val@karpov.io>
2026-03-11 22:02:43 -0400

    display tables in chat tab

--- backend/actions/ChatThread/createChatMessage.js
+++ backend/actions/ChatThread/createChatMessage.js
@@ -134,6 +134,8 @@ If the user's query is best answered with a chart, return a Chart.js 4 configura
 
 If the user\'s query is best answered by a map, return an object { $featureCollection } which contains a GeoJSON FeatureCollection
 
+If the user's query is best answered by a table, return an object { $table: { columns: string[], rows: any[][] } }
+
 Example output:
 
 The following script counts the number of users which are not deleted.
