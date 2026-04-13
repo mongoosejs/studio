7c783b7fb77e98ceaea056ba82c02bdb6b735de4
Valeri Karpov <val@karpov.io>
2026-04-12 21:51:25 -0400

    correct TypeScript params for creating express router

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,12 @@
 declare module '@mongoosejs/studio' {
   import { RequestHandler } from 'express';
+  import mongoose from 'mongoose';
 
-  const express: (path: string) => RequestHandler;
+  const express: (
+    path: string,
+    connOrMongoose?: mongoose.Connection | mongoose.Mongoose,
+    options?: { apiKey?: string }
+  ) => RequestHandler;
 
   const studio: {
     express: typeof express;
